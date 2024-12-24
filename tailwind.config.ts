1d10815183bb0bf0a233ba592ee4ef126696f339
Ytalo-Alves <Ytaloalves10@hotmail.com>
2024-12-24 11:34:10 -0300

    Starting the in Bio project to share projects with social networks

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "BACKGROUND-PRIMARY": "#050505",
+        "BACKGROUND-SECONDARY": "#0F0F10",
+        "BACKGROUND-TERTIARY": "#19191A",
+        "CONTENT-BODY": "#CDCBCC",
+        "CONTENT-PLACEHOLDER": "#827D7F",
+        "CONTENT-HEADLINE": "#B2B2B2",
+        "BORDER-PRIMARY": "#19191A",
+        "BORDER-SECONDARY": "#323234",
+        "BORDER-TERTIARY": "#97979B",
+        "ACCENT-PURPLE": "#4B2DBB",
+        "ACCENT-GREEN": "#87BB2D",
+        "ACCENT-PINK": "#B5446B"
       },
     },
   },
